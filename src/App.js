db69e3659daa42b722d2a89ec6fd865a1960d5e1
tarielgafurov <tarielgafurov618@gmail.com>
2022-10-21 16:10:58 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Input from './components/UI/Input';
 
 
 function App() {
   return (
     <div className="App">
+      <Input type="text" placeholder="Ваше имя" />
     </div>
   );
 }
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Input = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Input = ({onChange , placeholder}) => {
+  return <StyledInput type="text" onChange={onChange} placeholder={placeholder} />
 }
+
+const StyledInput = styled.input`
+width: 280px;
+height: 30px;
+background-color: #0B345A;
+border-radius: 2px;
+outline: none;
+border: none;
+color: white;
+padding: 5px 5px 5px 10px;
+&::placeholder {
+      height: 15px;
+      font-family: 'Inter';
+      font-size: 13.5px;
+      color: #c4c4c4;
+   }
+`
 
 export default Input
