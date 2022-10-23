02f1742f864feaabe47303408fe0f9405478525b
User <user@MacBook-Pro-4.local>
2022-10-24 00:45:02 +0600

    add map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import AboutUs from "./components/AboutUs";
 import Footer from "./components/footer/Footer";
 import Header from "./components/headers/Header";
 import Infonrmation from "./components/Information";
+import Map from "./components/Map";
 import OurCourses from "./components/OurCourses";
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Header />
       <Infonrmation />
       <OurCourses />
+      <AboutUs array={[""]} />
+      <Map />
       <Footer />
     </div>
   );
